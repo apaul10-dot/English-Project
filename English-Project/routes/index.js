0fc4611045aa6c6e059aa8d426725b7886f27c7e
Arkajit Paul <arkajitpaul@gmail.com>
2025-05-15 20:44:59 -0400

    Updated food insecurity page with research abstract and introduction, enhanced content organization and styling

--- English-Project/routes/index.js
+++ English-Project/routes/index.js
@@ -11,4 +11,24 @@ router.get('/learn', (req, res) => {
   });
 });
 
+// Food Insecurity route
+router.get('/learn/food-insecurity', (req, res) => {
+  res.render('learn/food-insecurity', { 
+    title: 'Food Insecurity',
+    isAuthenticated: req.isAuthenticated(),
+    user: req.user,
+    active: 'learn'
+  });
+});
+
+// About route
+router.get('/about', (req, res) => {
+  res.render('about', { 
+    title: 'About Our Research Team',
+    isAuthenticated: req.isAuthenticated(),
+    user: req.user,
+    active: 'about'
+  });
+});
+
 module.exports = router; 
